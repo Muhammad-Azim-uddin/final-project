d6c62649f95daaabfd98fcc42425f487a4bbdf53
azimuddin <azim.uddin.rabbi@gmail.com>
2024-12-18 23:59:00 +0600

    features and 2nd  banner section

--- js file/main.js
+++ js file/main.js
@@ -16,3 +16,18 @@ $(".dropdown-toggle").click(function(){
     $(".dropdown-menu").toggleClass("active");
 })
 var mixer = mixitup('.mixit');
+
+// --digital clock.. 
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    $('#hours').text(hours);
+    $('#minutes').text(minutes);
+    $('#seconds').text(seconds);
+}
+
+setInterval(updateClock, 1000);
+updateClock(); 
